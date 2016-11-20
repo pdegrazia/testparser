6f2e893c471fbd43ed89da958e86b0ceae799339
Paolo <p.degrazia@gmail.com>
2016-11-20 00:34:12 +0000

    changed template to match jinja2

--- templates.py
+++ templates.py
@@ -1,2 +1,24 @@
-HEAD = '<html><body><h1> Summary </h1>'
-FOOTER = '</body></html>'
+JINJA_TEMPLATE = '''
+<html>
+    <body>
+        <h1> Summary </h1>
+        <ul>
+        {% for test_suite in test_suites %}
+            <li>{{ test_suite.name }}
+            <ul>
+            {% for test_case in test_suite.testcase_table %}
+                <li>{{ test_case.name }}
+                    <ol>
+                        {% for test_step in test_case.steps %}
+                        <li>{{ test_step.keyword }}</li>
+                        {% endfor %}
+                    </ol>
+                </li>
+            {% endfor %}
+            </ul>
+        </li>
+        {% endfor %}
+        </ul>
+    </body>
+</html>
+'''
