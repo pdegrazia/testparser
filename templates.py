e830264a6c6b9827b6f8c574646aa018da2440ee
Fernando Castaneda <fernando.castaneda@workshare.com>
2018-02-07 14:45:38 +0000

    [templates.py] Filtering by category

--- templates.py
+++ templates.py
@@ -7,6 +7,15 @@ JINJA_TEMPLATE = '''
             .button-qa-ws {
                 background-color: yellow;
             }
+            .filterDiv {
+                display: none;
+                }
+            .show {
+                display: block;
+            }
+            .btn:hover {
+                background-color: #ddd;
+            }
         </style>
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -14,9 +23,9 @@ JINJA_TEMPLATE = '''
     </head>
     <body>
         <h1 style="text-align:center;background-color:#EF632F;color:White;font-family:Merriweather"> Regression pack summary </h1>
-        <div id="myBtnContainer"  style="background-color:#F2F2F2;">
+        <div id="myBtnContainer">
             <button class="btn active" onclick="filterSelection('all')"> Show all</button>
-            <button class="btn" onclick="filterSelection('fe')"> FE</button>
+            <button class="btn" onclick="filterSelection('web')"> FE</button>
             <button class="btn" onclick="filterSelection('api')"> API</button>
             <button class="btn" onclick="filterSelection('events')"> EVENTS</button>
         </div>
@@ -45,11 +54,12 @@ JINJA_TEMPLATE = '''
             <ul class="list-group">
             {% for test_suite in test_suites %}
             {% set suite_loop = loop %}
-                <li class="list-group-item" style="background-color:#F2F2F2;">
+                <div class= "filterDiv {% if "web" in test_suite.setting_table.force_tags.value %}web{% elif "api" in test_suite.setting_table.force_tags.value %}api{% elif "events" in test_suite.setting_table.force_tags.value %}events{% endif %}" style="background-color:#F2F2F2;">
+                <li class="list-group-item">
                       <div class="card">
                         <div class="card-header" id="suite-heading-{{suite_loop.index}}">
                           <h5 class="mb-0">
-                            <button class="btn btn-link collapsed" style="color:Black;" data-toggle="collapse" data-target="#collapse-{{suite_loop.index}}" aria-expanded="false" aria-controls="collapse-{{suite_loop.index}}">
+                            <button class="btn btn-link collapsed" style="color:Black;font-family:Lato;" data-toggle="collapse" data-target="#collapse-{{suite_loop.index}}" aria-expanded="false" aria-controls="collapse-{{suite_loop.index}}">
                               {{ test_suite.name }}
                                     {% for tag in test_suite.setting_table.force_tags.value %}
                                         <span class="badge badge-pill badge-secondary">{{tag}}</span>
@@ -88,12 +98,57 @@ JINJA_TEMPLATE = '''
                         </div>
                       </div>
                 </li>
+                </div>
             {% endfor %}
             </ul>
         </div>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script>
+            filterSelection("all")
+            function filterSelection(c) {
+              var x, i;
+              x = document.getElementsByClassName("filterDiv");
+              if (c == "all") c = "";
+              for (i = 0; i < x.length; i++) {
+                w3RemoveClass(x[i], "show");
+                if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+              }
+            }
+
+            function w3AddClass(element, name) {
+              var i, arr1, arr2;
+              arr1 = element.className.split(" ");
+              arr2 = name.split(" ");
+              for (i = 0; i < arr2.length; i++) {
+                if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+              }
+            }
+
+            function w3RemoveClass(element, name) {
+              var i, arr1, arr2;
+              arr1 = element.className.split(" ");
+              arr2 = name.split(" ");
+              for (i = 0; i < arr2.length; i++) {
+                while (arr1.indexOf(arr2[i]) > -1) {
+                  arr1.splice(arr1.indexOf(arr2[i]), 1);
+                }
+              }
+              element.className = arr1.join(" ");
+            }
+
+            // Add active class to the current button (highlight it)
+            var btnContainer = document.getElementById("myBtnContainer");
+            var btns = btnContainer.getElementsByClassName("btn");
+            for (var i = 0; i < btns.length; i++) {
+              btns[i].addEventListener("click", function(){
+                var current = document.getElementsByClassName("active");
+                current[0].className = current[0].className.replace(" active", "");
+                this.className += " active";
+              });
+            }
+        </script>
 
     </body>
 </html>
